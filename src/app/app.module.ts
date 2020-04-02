4568a8c4440408f764b070b3dd49e5c50fdae329
Iana Stepanova <Iana_Stepanova@epam.com>
2020-04-02 16:24:23 +0300

    added navbar  and route paths to employee-list and add-employee components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,9 +8,19 @@ import { ListEmployeesComponent } from './employees/list-employees.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
 
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  {path: '', redirectTo: "/list", pathMatch: 'full'}
+];
 @NgModule({
   declarations: [AppComponent, ListEmployeesComponent, CreateEmployeeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
