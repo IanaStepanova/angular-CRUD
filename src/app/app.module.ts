703193a5d3274b10b4ddce3cbce1cca6f1b07249
Iana Stepanova <Iana_Stepanova@epam.com>
2020-04-04 21:59:47 +0300

    implemented two-way binding in the form input fields and a mock-up SaveEmployee method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +12,7 @@ import { CreateEmployeeComponent } from './employees/create-employee.component';
 const appRoutes: Routes = [
   { path: 'list', component: ListEmployeesComponent },
   { path: 'create', component: CreateEmployeeComponent },
-  {path: '', redirectTo: "/list", pathMatch: 'full'}
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
 ];
 @NgModule({
   declarations: [AppComponent, ListEmployeesComponent, CreateEmployeeComponent],
@@ -19,6 +20,7 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [],
